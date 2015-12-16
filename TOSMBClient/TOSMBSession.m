0a0a49ecd09db3f45b0f5e9a787d18f89afef610
Tim Oliver <me@timoliver.com.au>
2015-12-16 20:29:59 +0800

    Reverted null credential values.

--- TOSMBClient/TOSMBSession.m
+++ TOSMBClient/TOSMBSession.m
@@ -220,8 +220,8 @@
     
     //If the username or password wasn't supplied, a non-NULL string must still be supplied
     //to avoid NULL input assertions.   
-    const char *userName = (self.userName ? [self.userName cStringUsingEncoding:NSUTF8StringEncoding] : '\0');
-    const char *password = (self.password ? [self.password cStringUsingEncoding:NSUTF8StringEncoding] : '\0');
+    const char *userName = (self.userName ? [self.userName cStringUsingEncoding:NSUTF8StringEncoding] : " ");
+    const char *password = (self.password ? [self.password cStringUsingEncoding:NSUTF8StringEncoding] : " ");
     
     //Attempt a login. Even if we're downgraded to guest, the login call will succeed
     smb_session_set_creds(session, hostName, userName, password);
