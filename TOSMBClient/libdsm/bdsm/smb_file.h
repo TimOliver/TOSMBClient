739ba72c10f6ed1ce7a78ac11e8c6374b51876f0
Tim Oliver <me@timoliver.com.au>
2016-01-24 15:38:30 +0800

    Updated libdsm static binary

--- TOSMBClient/libdsm/bdsm/smb_file.h
+++ TOSMBClient/libdsm/bdsm/smb_file.h
@@ -81,11 +81,25 @@ void      smb_fclose(smb_session *s, smb_fd fd);
 ssize_t   smb_fread(smb_session *s, smb_fd fd, void *buf, size_t buf_size);
 
 /**
- * @brief Sets/Moves/Get the read pointer for a given file
+ * @brief Write to an open file
+ * @details At most 'buf_size' bytes from memory pointed by 'buf' are written
+ * to the current seek offset of the open file represented by the smb file
+ * descriptor 'fd'.
+ *
+ * @param[in] s The session object
+ * @param[in] fd [description]
+ * @param[out] buf [description]
+ * @param[in] buf_size [description]
+ * @return The number of bytes written or -1 in case of error.
+ */
+ssize_t   smb_fwrite(smb_session *s, smb_fd fd, void *buf, size_t buf_size);
+
+/**
+ * @brief Sets/Moves/Get the read/write pointer for a given file
  * @details The behavior of this function is the same as the Unix fseek()
  * function, except the SEEK_END argument isn't supported.
  *
- * This functions adjust the read the read pointer depending on the value of
+ * This functions adjust the read/write pointer depending on the value of
  * offset and whence.
  *
  * - If whence == #SMB_SEEK_SET, the read pointer is set at 'offset'
@@ -96,7 +110,7 @@ ssize_t   smb_fread(smb_session *s, smb_fd fd, void *buf, size_t buf_size);
  * @param offset Set/Adjust quantity
  * @param whence Which action to perform. Supported operations are
  * #SMB_SEEK_SET and #SMB_SEEK_CUR
- * @return The current read pointer position
+ * @return The current read pointer position or -1 on error
  */
 ssize_t   smb_fseek(smb_session *s, smb_fd fd, ssize_t offset, int whence);
 
@@ -110,5 +124,17 @@ ssize_t   smb_fseek(smb_session *s, smb_fd fd, ssize_t offset, int whence);
  * @return 0 if delete OK or "NT" error code
  */
 uint32_t  smb_file_rm(smb_session *s, smb_tid tid, const char *path);
+
+/**
+ * @brief move/rename a file/directory on a share.
+ * @details Use this function to move and/or rename a file/directory
+ *
+ * @param s The session object
+ * @param tid The tid of the share the file is in, obtained via smb_tree_connect()
+ * @param old_path The current path of the file/directory to move/rename
+ * @param new_path The new path of the file/directory
+ * @return 0 if move OK or -1 in case of error
+ */
+int       smb_file_mv(smb_session *s, smb_tid tid, const char *old_path, const char *new_path);
 
 #endif
